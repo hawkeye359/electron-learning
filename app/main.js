b9046abc2111629ad97d56cd7aa1fee2e4fa429c
huzaifa <mohdhuzaifa359@gmail.com>
2021-11-02 13:06:33 +0530

    nothing

--- app/main.js
+++ app/main.js
@@ -1,1 +1,30 @@
-console.log('This is an Electron application!');
+const { app, BrowserWindow, dialog } = require("electron");
+const fs = require("fs").promises;
+let mainWindow = null;
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({ show: false });
+  mainWindow.loadFile(`${__dirname}/index.html`);
+  mainWindow.once("ready-to-show", () => {
+    mainWindow.show();
+  });
+});
+
+exports.openFileDialog = () => {
+  const files = dialog.showOpenDialog({
+    properties: ["openFile"],
+    filters: [
+      { name: "TextFile", extensions: ["txt"] },
+      { name: "MarkdownFiles", extensions: ["md"] },
+    ],
+    buttonLabel: "Unveil",
+    title: "FireSale",
+  });
+  if (!files) return;
+  openFileContent(files[0]);
+};
+const openFileContent = async (file) => {
+  const content = await fs.readFile(file);
+  const contentString = content.toString();
+  mainWindow.webContents.send("file-opened", file, contentString);
+};
--- app/renderer.js
+++ app/renderer.js
@@ -1,20 +1,46 @@
-const marked = require('marked');
+const marked = require("marked");
+const path = require("path");
+let filePath = null;
+let originalContent = "";
+const { remote, ipcRenderer } = require("electron");
+const { openFileDialog } = remote.require("./main.js");
+const currentWindow = remote.getCurrentWindow();
+const markdownView = document.querySelector("#markdown");
+const htmlView = document.querySelector("#html");
+const newFileButton = document.querySelector("#new-file");
+const openFileButton = document.querySelector("#open-file");
+const saveMarkdownButton = document.querySelector("#save-markdown");
+const revertButton = document.querySelector("#revert");
+const saveHtmlButton = document.querySelector("#save-html");
+const showFileButton = document.querySelector("#show-file");
+const openInDefaultButton = document.querySelector("#open-in-default");
 
-const markdownView = document.querySelector('#markdown');
-const htmlView = document.querySelector('#html');
-const newFileButton = document.querySelector('#new-file');
-const openFileButton = document.querySelector('#open-file');
-const saveMarkdownButton = document.querySelector('#save-markdown');
-const revertButton = document.querySelector('#revert');
-const saveHtmlButton = document.querySelector('#save-html');
-const showFileButton = document.querySelector('#show-file');
-const openInDefaultButton = document.querySelector('#open-in-default');
-
-const renderMarkdownToHtml = markdown => {
+const renderMarkdownToHtml = (markdown) => {
   htmlView.innerHTML = marked(markdown, { sanitize: true });
 };
 
-markdownView.addEventListener('keyup', event => {
+markdownView.addEventListener("keyup", (event) => {
   const currentContent = event.target.value;
   renderMarkdownToHtml(currentContent);
+  upDateUI(currentContent !== originalContent);
 });
+openFileButton.addEventListener("click", () => {
+  openFileDialog();
+});
+ipcRenderer.on("file-opened", (e, file, content) => {
+  filePath = file;
+  originalContent = content;
+  markdownView.value = content;
+  renderMarkdownToHtml(content);
+  upDateUI();
+});
+const upDateUI = (isEdited) => {
+  let title = "Fire Sale";
+  if (filePath) {
+    title = `${path.basename(filePath)}-${title}`;
+    if (isEdited) {
+      title = "• " + title;
+    }
+  }
+  currentWindow.setTitle(title);
+};
